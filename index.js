4e46e53550b7f6ffd2e125bf2e5b334110bb37a2
Aly Badara Coulibaly <alybadara1803@gmail.com>
2025-07-12 12:33:16 +0100

    update

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // 1. Create two variables, firstCard and secondCard. 
 // Set their values to a random number between 2-11
-
+let name = prompt("Welcome, please enter your name : ")
+//initialise
+//listen
+//change
+//increment
+let nameEL = document.getElementById("name-el")
+nameEL.textContent = "welcome Mister" + nameEL
 let firstCard = 3
 let secondCard = 10
 // 2. Create a variable, sum, and set it to the sum of the two cards
