7cc5f4442dbb48e7631096bbf31f3b2b0d038e80
Aly Badara Coulibaly <alybadara1803@gmail.com>
2025-07-13 20:47:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 // 1. Create two variables, firstCard and secondCard. 
 // Set their values to a random number between 2-11
-let name = prompt("Welcome, please enter your name : ")
-//initialise
-//listen
-//change
-//increment
-let nameEL = document.getElementById("name-el")
-nameEL.textContent = "welcome Mister" + " " + name
+// let name = prompt("Welcome, please enter your name : ")
+// //initialise
+// //listen
+// //change
+// //increment
+// let nameEL = document.getElementById("name-el")
+// nameEL.textContent = "welcome Mister" + " " + name
 let firstCard = 3
 let secondCard = 10
 // 2. Create a variable, sum, and set it to the sum of the two cards
 let sum = firstCard + secondCard
+
+if (sum < 21 ){
+    console.log("Do you want to draw a new card ?")
+} else if ( sum === 21 ){
+    console.log("You've won the BlackJack !")
+} else {
+    console.log('You\'ve lost')
+}
