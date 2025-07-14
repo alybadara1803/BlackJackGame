07956148eb84416467f6278ac09deb1034206955
Aly Badara Coulibaly <alybadara1803@gmail.com>
2025-07-14 06:58:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ let secondCard = 7
 // 2. Create a variable, sum, and set it to the sum of the two cards
 let sum = firstCard + secondCard
 
-if (sum < 21 ){
+if (sum <= 20 ){
     console.log("Do you want to draw a new card ?")
 } else if ( sum === 21 ){
     console.log("You've won the BlackJack !")
 } else {
-    console.log('You\'ve lost')
+    console.log('You\'re out of the game')
 }
 
