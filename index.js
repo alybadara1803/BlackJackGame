2a3f63c47c901db7246420edf6c8b0d02643c869
Aly Badara Coulibaly <alybadara1803@gmail.com>
2025-07-21 08:16:43 +0100

    Update

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 let firstCard = 10
-let secondCard = 4
+let secondCard = 11
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 
 let messageEl = document.getElementById("message-el")
+// 2. Store the sum paragraph in a variable called sumEl
+let sumEl = document.getElementById("sum-el") 
+// let sumEl = document.querySelector("#sum-el")
+
+let cardsEl = document.querySelector("#cards-el")
 
 function startGame(){
+    cardsEl.textContent = "Cards : " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum : " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card? 🙂"
     } else if (sum === 21) {
@@ -19,11 +26,5 @@ function startGame(){
     }
 
     messageEl.textContent = message
-    console.log(message)
-
 }
 
-//initialise
-//listen
-//change
-//display
